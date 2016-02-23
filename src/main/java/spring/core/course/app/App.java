ab4b2f86f6eaea85d10c5f5215c08b2eab200234
Ivan Pronin <Ivan_Pronin@epam.com>
2016-02-23 13:29:15 +0300

    Added sample App application

---
+++ src/main/java/spring/core/course/app/App.java
@@ -0,0 +1,22 @@
+package spring.core.course.app;
+
+public class App
+{
+
+    private Client client;
+    private ConsoleEventLogger logger;
+
+    public static void main(String[] args)
+    {
+        App app = new App();
+        app.client = new Client("1", "John Smith");
+        app.logger = new ConsoleEventLogger();
+        app.logEvent("Some event for client 1");
+    }
+
+    private void logEvent(String msg)
+    {
+        String message = msg.replaceAll(client.getId(), client.getFullName());
+        logger.logEvent(message);
+    }
+}
---
+++ src/main/java/spring/core/course/app/ConsoleEventLogger.java
@@ -0,0 +1,10 @@
+package spring.core.course.app;
+
+public class ConsoleEventLogger
+{
+
+    public void logEvent(String msg)
+    {
+        System.out.println("Logging: " + msg);
+    }
+}
